004aab1002f51d13fde10f1914c80c2775dee267
afsdl <saucedodeleonariel@cticoban.edu.gt>
2023-05-30 17:49:02 -0600

    Se agrego el proyecto

---
+++ administrador/inicio.php
@@ -0,0 +1,16 @@
+<?php include('template/cabecera.php');?>
+        <div class="col-md-12">
+            
+        <div class="jumbotron">
+    <h1 class="display-3">Bienvenido <?php echo $nombreUsuario; ?> </h1>
+    <p class="lead">En esta seccion se podran hacer modificaciones de los productos</p>
+    <hr class="my-2">
+    <p>More info</p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="seccion/productos.php" role="button">Administrar Productos</a>
+    </p>
+  </div>
+
+        </div>
+        <?php include('template/pie.php');?>
+   
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include("template/cabecera.php");?>
+
+    <div class="jumbotron">
+        <h1 class="display-3">Bienvenidos al Sitio Web</h1>
+        <p class="lead">En esta seccion podras ver los productos</p>
+        <hr class="my-2">
+        <p>Más información</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver Almacén de Productos</a>
+        </p>
+    </div>
+    <?php include("template/pie.php");?>
